ff23996e36c8109bb729bccb9b1b94e0cc431511
dsouflis <dsouflis@gmail.com>
2024-08-02 22:04:07 +0300

    More work on validity

--- spec/validity.spec.ts
+++ spec/validity.spec.ts
@@ -3,10 +3,33 @@ import {
   ValidityLeftClosed,
   ValidityLeftOpen,
   Context,
-  combineValidities
+  combineValidities,
+  contextCommensurate,
 } from '../validity.js';
 
-describe('combineValidities', () => {
+describe('contextCommensurate', () => {
+  it('returns false for contexts on different branches', () => {
+    const c1 = ['', 'h1'];
+    const c2 = ['', 'h2'];
+    const b = contextCommensurate(c1, c2);
+    expect(b).to.false;
+
+    const b2 = contextCommensurate(c2, c1);
+    expect(b2).to.false;
+  });
+
+  it('returns true for contexts on the same branch', () => {
+    const c1 = ['', 'h1'];
+    const c2 = ['', 'h1', 'h2'];
+    const b = contextCommensurate(c1, c2);
+    expect(b).to.true;
+
+    const b2 = contextCommensurate(c2, c1);
+    expect(b2).to.true;
+  });
+});
+
+describe('combineValidities on the same branch', () => {
   it('works for v1 strictly before closed v2', () => {
     const v1 = new ValidityLeftClosed([''],['','h1']);
     const v2 = new ValidityLeftClosed(['','h1', 'h2'], ['', 'h1', 'h2', 'h3']);
@@ -100,6 +123,104 @@ describe('combineValidities', () => {
     const v1 = new ValidityLeftOpen(['','h1']);
     const combined = combineValidities(v1, v2);
     expect(combined).to.eql(new ValidityLeftClosed(['','h1'],['','h1', 'h2']));
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+});
+
+describe('combineValidities on different branch', () => {
+  it('works for completely incommensurate v1 and open v2', () => {
+    const v1 = new ValidityLeftClosed(['','h1'],['','h1', 'h2']);
+    const v2 = new ValidityLeftOpen(['','h2']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.null;
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for completely incommensurate open v1 and v2', () => {
+    const v1 = new ValidityLeftOpen(['','h2']);
+    const v2 = new ValidityLeftClosed(['','h1'],['','h1', 'h2']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.null;
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for completely incommensurate v1 and v2', () => {
+    const v1 = new ValidityLeftClosed(['','h2'],['','h2','h3']);
+    const v2 = new ValidityLeftClosed(['','h1'],['','h1', 'h2']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.null;
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for completely incommensurate open v1 and open v2', () => {
+    const v1 = new ValidityLeftOpen(['','h2']);
+    const v2 = new ValidityLeftOpen(['','h1']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.null;
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for open v1 and open v2 below it', () => {
+    const v1 = new ValidityLeftOpen(['','h1']);
+    const v2 = new ValidityLeftOpen(['','h1','h2']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(v2);
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for open v1 and open v2 above it', () => {
+    const v1 = new ValidityLeftOpen(['','h1','h2']);
+    const v2 = new ValidityLeftOpen(['','h1']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(v1);
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for open v1 and v2 below it', () => {
+    const v1 = new ValidityLeftOpen(['','h1']);
+    const v2 = new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(v2);
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for open v1 and v2 above it', () => {
+    const v1 = new ValidityLeftOpen(['','h1','h2']);
+    const v2 = new ValidityLeftClosed(['','h1'], ['','h1','h2','h3']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']));
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for v1 below open v2', () => {
+    const v1 = new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']);
+    const v2 = new ValidityLeftOpen(['','h1']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(v1);
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for v1 above open v2', () => {
+    const v1 = new ValidityLeftClosed(['','h1'], ['','h1','h2','h3']);
+    const v2 = new ValidityLeftOpen(['','h1','h2']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']));
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for v1 starting under v2', () => {
+    const v1 = new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']);
+    const v2 = new ValidityLeftClosed(['','h1'], ['','h1','h2','h4']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(new ValidityLeftClosed(['','h1','h2'], ['','h1','h2']));
+    console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
+  });
+
+  it('works for v1 starting over v2', () => {
+    const v1 = new ValidityLeftClosed(['','h1'], ['','h1','h2','h4']);
+    const v2 = new ValidityLeftClosed(['','h1','h2'], ['','h1','h2','h3']);
+    const combined = combineValidities(v1, v2);
+    expect(combined).to.eql(new ValidityLeftClosed(['','h1','h2'], ['','h1','h2']));
     console.log('combineValidities(',v1.toString(),',',v2.toString(),') =', combined?.toString() || 'null');
   });
 });
--- validity.ts
+++ validity.ts
@@ -59,31 +59,73 @@ function contextEqual(c1: Context, c2: Context): boolean {
   return c1.length === c2.length && equalToPrefix(c1, c2);
 }
 
+export function contextCommensurate(c1: Context, c2: Context): boolean {
+  for (let i = 0; i < Math.min(c1.length, c2.length); i++){
+    if(c2[i] !== c1[i]) return false;
+  }
+  return true;
+}
+
+function commonPrefix(c1: Context, c2: Context): Context {
+  let ret: Context = [];
+  for (let i = 0; i < c1.length; i++){
+    if(c2[i] === c1[i]) {
+      ret.push(c1[i]);
+    } else break;
+  }
+  return ret;
+}
+
 function validityStrictlyBefore(v1: Validity, v2: Validity): boolean {
   if(v1 instanceof ValidityLeftOpen) return false;
   return contextStrictlyBefore(v1.last, v2.first) ;
 }
 
-export function combineValidities(v1: Validity, v2: Validity): Validity | null {
-  if(!validityStrictlyBefore(v1, v2) && !validityStrictlyBefore(v2, v1)) {
-    if(v1 instanceof ValidityLeftClosed && contextEqual(v1.last, v2.first)) {
+function combineValiditiesOnSameBranch(v1: Validity, v2: Validity): Validity | null {
+  if (!validityStrictlyBefore(v1, v2) && !validityStrictlyBefore(v2, v1)) {
+    if (v1 instanceof ValidityLeftClosed && contextEqual(v1.last, v2.first)) {
       return new ValidityLeftClosed(v2.first, v2.first);
-    } else if(v2 instanceof ValidityLeftClosed && contextEqual(v2.last, v1.first)) {
+    } else if (v2 instanceof ValidityLeftClosed && contextEqual(v2.last, v1.first)) {
       return new ValidityLeftClosed(v1.first, v1.first);
-    } else if(v1 instanceof ValidityLeftClosed && contextStrictlyBefore(v2.first, v1.last)) {
+    } else if (v1 instanceof ValidityLeftClosed && contextStrictlyBefore(v2.first, v1.last)) {
       if (v2 instanceof ValidityLeftOpen) {
         return new ValidityLeftClosed(contextMax(v1.first, v2.first), v1.last);
       } else {
         return new ValidityLeftClosed(contextMax(v1.first, v2.first), contextMin(v1.last, v2.last));
       }
-    } else if(v2 instanceof ValidityLeftClosed && contextStrictlyBefore(v1.first, v2.last)) {
+    } else if (v2 instanceof ValidityLeftClosed && contextStrictlyBefore(v1.first, v2.last)) {
       if (v1 instanceof ValidityLeftOpen) {
         return new ValidityLeftClosed(contextMax(v1.first, v2.first), v2.last);
       } else {
         return new ValidityLeftClosed(contextMax(v1.first, v2.first), contextMin(v1.last, v2.last));
       }
-    } else if(v1 instanceof ValidityLeftOpen && v2 instanceof ValidityLeftOpen) {
+    } else if (v1 instanceof ValidityLeftOpen && v2 instanceof ValidityLeftOpen) {
       return new ValidityLeftOpen(contextMax(v1.first, v2.first));
+    }
+  }
+  return null;
+}
+
+function combineValiditiesOnDiffBranches(v1: Validity, v2: Validity): Validity | null {
+  if(v1 instanceof ValidityLeftOpen && v2 instanceof ValidityLeftOpen) {
+    return new ValidityLeftOpen(contextMax(v1.first, v2.first));
+  } else if(v1 instanceof ValidityLeftOpen && v2 instanceof ValidityLeftClosed) {
+    return new ValidityLeftClosed(contextMax(v1.first, v2.first), v2.last);
+  } else if(v2 instanceof ValidityLeftOpen && v1 instanceof ValidityLeftClosed) {
+    return new ValidityLeftClosed(contextMax(v1.first, v2.first), v1.last);
+  } else if(v1 instanceof ValidityLeftClosed && v2 instanceof ValidityLeftClosed) {
+    const cmnPrefix = commonPrefix(v1.last, v2.last);
+    return new ValidityLeftClosed(contextMax(v1.first, v2.first), cmnPrefix);
+  }
+  return null;
+}
+
+export function combineValidities(v1: Validity, v2: Validity): Validity | null {
+  if (contextCommensurate(v1.first, v2.first)) { //share at least some path
+    if (v1 instanceof ValidityLeftOpen || v2 instanceof ValidityLeftOpen || contextCommensurate(v1.last, v2.last)) { //on same branch
+      return combineValiditiesOnSameBranch(v1, v2);
+    } else {
+      return combineValiditiesOnDiffBranches(v1, v2);
     }
   }
   return null;
